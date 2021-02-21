5f014711c1e442152ba2f08bee8ea007943a58c4
Paulius B <ppbernotas@gmail.com>
2021-02-22 00:00:29 +0200

    - Added a config file option to "UpdateUI", disabled by default, which will stop the recipe requirements from flashing red when player does not have enough items in the inventory, but there are enough in the nearby containers.    + This uses a Transpiler Harmony patch to edit the following line in InventoryGui.SetupRequirement() function:    + int num = player.GetInventory().CountItems(req.m_resItem.m_itemData.m_shared.m_name);    + I've replaced that line with a call to a new function GetNumItemsInInventoryAndNearbyContainers() which will check the config flag, and either do the old behaviour, or check the containers as well. - Also added a config option "ShowConnections", disabled by default, which will connect the container with workstations that are within range, when using the hammer to place the containers.    + Couple configurable options for the effect start position on the container and delay for removal.    + This required finding the prefab for the connection vfx and instantiating it directly as containers dont have StationExtension component where the prefab usually lives    + The instantiated vfx objects are registered to a list to keep track of them and to clean them up properly.

--- CraftFromContainers/BepInExPlugin.cs
+++ CraftFromContainers/BepInExPlugin.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Reflection;
+using System.Reflection.Emit;
 using BepInEx;
 using BepInEx.Configuration;
 using HarmonyLib;
@@ -13,9 +14,23 @@ namespace CraftFromContainers
     {
         private static readonly bool isDebug = true;
 
+        private static BepInExPlugin instance = null;
+        private static List<ConnectionParams> containerConnections = new List<ConnectionParams>();
+        private static GameObject connectionVfxPrefab = null;
+
         public static ConfigEntry<float> m_range;
+        public static ConfigEntry<bool> updateItemUi;
+        public static ConfigEntry<bool> showGhostConnections;
+        public static ConfigEntry<float> ghostConnectionStartOffset;
+        public static ConfigEntry<float> ghostConnectionRemovalDelay;
         public static ConfigEntry<bool> modEnabled;
         public static List<Container> containerList = new List<Container>();
+
+        public class ConnectionParams
+        {
+            public GameObject connection = null;
+            public Vector3 stationPos;
+        }
 
         public static void Dbgl(string str = "", bool pref = true)
         {
@@ -24,13 +39,24 @@ namespace CraftFromContainers
         }
         private void Awake()
         {
+            instance = this;
+
             m_range = Config.Bind<float>("General", "ContainerRange", 10f, "The maximum range from which to pull items from");
+            updateItemUi = Config.Bind<bool>("General", "UpdateUI", false, "If enabled, will stop the UI flashing red if there are enough items within nearby containers");
+            showGhostConnections = Config.Bind<bool>("Station Connections", "ShowConnections", false, "If true, will display connections to nearby workstations within range when building containers");
+            ghostConnectionStartOffset = Config.Bind<float>("Station Connections", "ConnectionStartOffset", 1.25f, "Height offset for the connection VFX start position");
+            ghostConnectionRemovalDelay = Config.Bind<float>("Station Connections", "ConnectionRemoveDelay", 0.3f, "");
             modEnabled = Config.Bind<bool>("General", "enabled", true, "Enable this mod");
 
             if (!modEnabled.Value)
                 return;
 
             Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), null);
+        }
+
+        private void OnDestroy()
+        {
+            StopConnectionEffects();
         }
 
         public static List<Container> GetNearbyContainers(Vector3 center)
@@ -64,6 +90,130 @@ namespace CraftFromContainers
             static void Prefix(Container __instance)
             {
                 containerList.Remove(__instance);
+            }
+        }
+
+        static int GetNumItemsInInventoryAndNearbyContainers(Player player, Piece.Requirement requirement)
+        {
+            int totalAmount = 0;
+
+            List<Container> nearbyContainers = GetNearbyContainers(player.transform.position);
+
+            if (requirement.m_resItem)
+            {
+                totalAmount = player.GetInventory().CountItems(requirement.m_resItem.m_itemData.m_shared.m_name);
+             
+                if (updateItemUi.Value)
+                {
+                    foreach (Container c in nearbyContainers)
+                    {
+                        totalAmount += c.GetInventory().CountItems(requirement.m_resItem.m_itemData.m_shared.m_name);
+                    }
+                }
+            }
+
+            return totalAmount;
+        }
+
+        [HarmonyPatch(typeof(InventoryGui), "SetupRequirement")]
+        static class SetupRequirement_Patch
+        {
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                //Dbgl($"######## SetupRequirement_Patch START ########");
+                int getInventoryInstrIndex = -1;
+                var codes = new List<CodeInstruction>(instructions);
+                for (var i = 0; i < codes.Count; i++)
+                {
+                    CodeInstruction instr = codes[i];
+
+                    //Dbgl($"{i} {instr}");
+
+                    if (instr.opcode == OpCodes.Callvirt)
+                    {
+                        String instrString = instr.ToString();
+                        if (instrString.Contains("CountItems"))         // Looking for this line: int num = player.GetInventory().CountItems(req.m_resItem.m_itemData.m_shared.m_name);
+                        {
+                            for (var j = i-1; j >= 0; j--)              // From there navigate back to the first instruction that we want to replace: callvirt Humanoid::GetInventory()
+                            {
+                               // Dbgl($"^{j} {codes[j].ToString()}");
+
+                                if (codes[j].opcode == OpCodes.Callvirt)
+                                {
+                                    instrString = codes[j].ToString();
+                                    if (instrString.Contains("GetInventory()"))
+                                    {
+                                        getInventoryInstrIndex = j;
+                                        break;
+                                    }
+                                }
+                            }
+
+                            // Remove all instructions that are not loading the function arguments. We will reuse the same arguments, so we can keep those.
+                            if (getInventoryInstrIndex > -1)
+                            {
+                                //Dbgl($"Removing instruction at {getInventoryInstrIndex}: {codes[getInventoryInstrIndex].ToString()}");
+                                codes.RemoveAt(getInventoryInstrIndex);
+                                i--;
+                                for (var j = getInventoryInstrIndex; j <= codes.Count; j++)
+                                {
+                                    bool bLastInstruction = false;
+                                    instrString = codes[j].ToString();
+                                    if (instrString.Contains("CountItems"))
+                                    {
+                                        bLastInstruction = true;
+                                    }
+                                    
+                                    //Dbgl($"v{j} {codes[j].ToString()}");
+
+                                    if (codes[j].opcode != OpCodes.Ldarg &&
+                                        codes[j].opcode != OpCodes.Ldarg_S &&
+                                        codes[j].opcode != OpCodes.Ldarg_0 &&
+                                        codes[j].opcode != OpCodes.Ldarg_1 &&
+                                        codes[j].opcode != OpCodes.Ldarg_2 &&
+                                        codes[j].opcode != OpCodes.Ldarg_3)
+                                    {
+                                       // Dbgl($"Removing instruction at {j}: {codes[j].ToString()}");
+
+                                        codes.RemoveAt(j);
+                                        i--;
+                                        j--;
+                                    }
+                                    else
+                                    {
+                                        i++;
+                                    }
+
+                                    if (bLastInstruction)
+                                        break;
+                                }
+                            }
+
+                            // Insert a new instruction to call GetNumItemsInInventoryAndNearbyContainers(), which is going to be cached into same local variable as before.
+                            //Dbgl($"Inserting instruction at {i}:");
+                            //Dbgl($"Old: { codes[i].ToString()}");
+                            codes.Insert(i, new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(BepInExPlugin), "GetNumItemsInInventoryAndNearbyContainers")));
+                            //Dbgl($"New: { codes[i].ToString()}");
+                        }
+                    }
+                }
+
+                //Dbgl($"");
+                //Dbgl($"#############################################################");
+                //Dbgl($"######## MODIFIED INSTRUCTIONS - {codes.Count} ########");
+                //Dbgl($"#############################################################");
+                //Dbgl($"");
+                //
+                //for (var i = 0; i < codes.Count; i++)
+                //{
+                //    CodeInstruction instr = codes[i];
+                //
+                //    Dbgl($"{i} {instr}");
+                //}
+                //
+                //Dbgl($"######## SetupRequirement_Patch END ########");
+
+                return codes;
             }
         }
 
@@ -237,6 +387,126 @@ namespace CraftFromContainers
                 }
                 return false;
             }
+        }
+
+        [HarmonyPatch(typeof(Player), "UpdatePlacementGhost")]
+        static class UpdatePlacementGhost_Patch
+        {
+            static void Postfix(Player __instance, bool flashGuardStone)
+            {
+                if (!showGhostConnections.Value)
+                {
+                    return;
+                }
+
+                FieldInfo placementGhostField = typeof(Player).GetField("m_placementGhost", BindingFlags.Instance | BindingFlags.NonPublic);
+                GameObject placementGhost = placementGhostField != null ? (GameObject)placementGhostField.GetValue(__instance) : null;
+                if (placementGhost == null)
+                {
+                    return;
+                }
+
+                Container ghostContainer = placementGhost.GetComponent<Container>();
+                if (ghostContainer == null)
+                {
+                    return;
+                }
+
+                FieldInfo allStationsField = typeof(CraftingStation).GetField("m_allStations", BindingFlags.Static | BindingFlags.NonPublic);
+                List<CraftingStation> allStations = allStationsField != null ? (List<CraftingStation>)allStationsField.GetValue(null) : null;
+
+                if (connectionVfxPrefab == null)
+                {
+                    foreach (GameObject go in Resources.FindObjectsOfTypeAll(typeof(GameObject)) as GameObject[])
+                    {
+                        if (go.name == "vfx_ExtensionConnection")
+                        {
+                            connectionVfxPrefab = go;
+                            break;
+                        }
+                    }
+                }
+
+                if (connectionVfxPrefab == null)
+                {
+                    return;
+                }
+
+                if (allStations != null)
+                {
+                    bool bAddedConnections = false;
+                    foreach (CraftingStation station in allStations)
+                    {
+                        if (Vector3.Distance(station.transform.position, placementGhost.transform.position) < m_range.Value)
+                        {
+                            bAddedConnections = true;
+
+                            Vector3 connectionStartPos = station.GetConnectionEffectPoint();
+                            Vector3 connectionEndPos = placementGhost.transform.position + Vector3.up * ghostConnectionStartOffset.Value;
+
+                            ConnectionParams tempConnection = null;
+                            int connectionIndex = ConnectionExists(station);
+                            bool connectionAlreadyExists = connectionIndex == -1;
+                            if (connectionAlreadyExists)
+                            {
+                                tempConnection = new ConnectionParams();
+                                tempConnection.stationPos = station.GetConnectionEffectPoint();
+                                tempConnection.connection = UnityEngine.Object.Instantiate<GameObject>(connectionVfxPrefab, connectionStartPos, Quaternion.identity);
+                            }
+                            else
+                            {
+                                tempConnection = containerConnections[connectionIndex];
+                            }
+
+                            if (tempConnection.connection != null)
+                            {
+                                Vector3 vector3 = connectionEndPos - connectionStartPos;
+                                Quaternion quaternion = Quaternion.LookRotation(vector3.normalized);
+                                tempConnection.connection.transform.position = connectionStartPos;
+                                tempConnection.connection.transform.rotation = quaternion;
+                                tempConnection.connection.transform.localScale = new Vector3(1f, 1f, vector3.magnitude);
+                            }
+
+                            if (connectionAlreadyExists)
+                            {
+                                containerConnections.Add(tempConnection);
+                            }
+                        }
+                    }
+
+                    if (bAddedConnections && instance != null)
+                    {
+                        instance.CancelInvoke("StopConnectionEffects");
+                        instance.Invoke("StopConnectionEffects", ghostConnectionRemovalDelay.Value);
+                    }
+                }
+            }
+        }
+
+        public static int ConnectionExists(CraftingStation station)
+        {
+            foreach (ConnectionParams c in containerConnections)
+            {
+                if (Vector3.Distance(c.stationPos, station.GetConnectionEffectPoint()) < 0.1f)
+                {
+                    return containerConnections.IndexOf(c);
+                }
+            }
+
+            return -1;
+        }
+
+        public void StopConnectionEffects()
+        {
+            if (containerConnections.Count > 0)
+            {
+                foreach (ConnectionParams c in containerConnections)
+                {
+                    UnityEngine.Object.Destroy((UnityEngine.Object)c.connection);
+                }
+            }
+
+            containerConnections.Clear();
         }
     }
 }
